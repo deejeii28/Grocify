a432957dc78e30f9e740a3d092070e4b05ab5ab6
Dustin John Redaon <dustin.redaon@gmail.com>
2026-04-19 19:44:14 +0800

    prefinal exam

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'white', fontSize: 30 }}>
+        Dustin John D. Redaon BSIT-2A
+      </Text>
+    </View>
+  );
+}
